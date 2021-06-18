fc19309187da185560f98df3280619006fb6c5c6
prasanth <prashanth9.namala@gmail.com>
2021-06-18 12:49:09 +0530

    dashboard table

--- src/app/layout/dashboard/dashboard.module.ts
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 const routes: Routes = [{
   path: '',
   component: DashboardComponent
@@ -11,7 +11,8 @@ const routes: Routes = [{
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    NgxDatatableModule
   ],
   declarations: [DashboardComponent]
 })
